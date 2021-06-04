2e01fff92efb7cbe1fa1961824ac2e72e10a2751
avy123 <avyudaya1@gmail.com>
2021-06-04 10:46:04 +0545

    added lead_create method to create new leads

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Lead
+from django.shortcuts import redirect, render
+from .models import Lead, Agent
+from .forms import LeadForm
 
 def lead_list(request):
   leads = Lead.objects.all()
@@ -14,3 +15,25 @@ def lead_detail(request, pk):
     'lead': lead
   }
   return render(request, 'leads/lead_detail.html', context)
+
+def lead_create(request):
+  form = LeadForm()
+  if request.method == 'POST':
+    form = LeadForm(request.POST)
+    if form.is_valid():
+      first_name = form.cleaned_data['first_name']
+      last_name = form.cleaned_data['last_name']
+      age = form.cleaned_data['age']
+      agent = Agent.objects.first()
+      Lead.objects.create(
+        first_name=first_name,
+        last_name=last_name,
+        age=age,
+        agent=agent
+      )
+      print('The lead has been created')
+      return redirect('/leads')
+  context = {
+    "form": form
+  }
+  return render(request, "leads/lead_create.html", context)
