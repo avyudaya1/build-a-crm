1cd943bd01b6ef50924632fcd0bc2d99e5e9a1c2
avy123 <avyudaya1@gmail.com>
2021-06-06 07:24:40 +0545

    added agent-detail_update_delete views

--- agents/views.py
+++ agents/views.py
@@ -6,16 +6,23 @@ from .forms import AgentModelForm
 
 class AgentListView(LoginRequiredMixin, generic.ListView):
   template_name = "agents/agent_list.html"
+  context_object_name = "agents"
   
   def get_queryset(self):
-      return Agent.objects.all()
+    return Agent.objects.all()
 
+class AgentDetailView(LoginRequiredMixin, generic.DetailView):
+  template_name = "agents/agent_detail.html"
+  context_object_name="agent"
+
+  def get_queryset(self):
+    return Agent.objects.all()
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
   template_name="agents/agent_create.html"
   form_class=AgentModelForm
 
   def get_success_url(self):
-      return reverse("agents:agent-list")
+    return reverse("agents:agent-list")
 
   def form_valid(self, form):
     agent = form.save(commit=False)
@@ -23,6 +30,22 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
     agent.save()
     return super(AgentCreateView, self).form_valid(form)
 
+class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
+  template_name="agents/agent_update.html"
+  form_class = AgentModelForm
 
+  def get_success_url(self):
+    return reverse("agents:agent-list")
 
+  def get_queryset(self):
+    return Agent.objects.all()
+
+class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
+  template_name = "agents/agent_delete.html"
+  context_object_name="agent"
   
+  def get_queryset(self):
+    return Agent.objects.all()
+
+  def get_success_url(self):
+    return reverse('agents:agent-list')
