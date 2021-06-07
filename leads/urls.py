c9b5e199d8eb313b44313bc5c5c4bc9c108f3b9b
avy123 <avyudaya1@gmail.com>
2021-06-07 18:19:10 +0545

    assign leads to an agent if we are logged in as organiser

--- leads/urls.py
+++ leads/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    lead_list, lead_detail, lead_create, lead_update, lead_delete,
+    AssignAgentView, lead_list, lead_detail, lead_create, lead_update, lead_delete,
     LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView
 )
 
@@ -10,7 +10,8 @@ app_name = 'leads'
 urlpatterns = [
     path('', LeadListView.as_view(), name='lead-list'),
     path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
-    path('create/', LeadCreateView.as_view(), name='lead-create'),
+    path('<int:pk>/assign-agent/', AssignAgentView.as_view(), name='assign-agent'),
     path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
-    path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete')
+    path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
+    path('create/', LeadCreateView.as_view(), name='lead-create')
 ]
